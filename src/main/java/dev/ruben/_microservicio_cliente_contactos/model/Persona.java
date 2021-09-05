db7dd2f654c4a164a0ff311caefea7f0286aaa40
Rubén Segura <rsegrom@gmail.com>
2021-09-05 19:25:44 +0200

    Creación del modelo pojo Persona

---
+++ src/main/java/dev/ruben/_microservicio_cliente_contactos/model/Persona.java
@@ -0,0 +1,65 @@
+package dev.ruben._microservicio_cliente_contactos.model;
+
+import java.util.Objects;
+
+public class Persona {
+
+    String nombre;
+    String email;
+    int edad;
+
+    public Persona() {
+    }
+
+    public Persona(String nombre, String email, int edad) {
+        this.nombre = nombre;
+        this.email = email;
+        this.edad = edad;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    @Override
+    public String toString() {
+        return "Persona{" +
+                "nombre='" + nombre + '\'' +
+                ", email='" + email + '\'' +
+                ", edad=" + edad +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Persona persona = (Persona) o;
+        return edad == persona.edad && Objects.equals(nombre, persona.nombre) && Objects.equals(email, persona.email);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(nombre, email, edad);
+    }
+}
